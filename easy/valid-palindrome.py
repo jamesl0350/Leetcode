e432d5e7e470fafd39a17efa1b9dde49e9ac5789
jamesl0350 <locklear.james.jl@gmail.com>
2025-03-20 14:19:40 -0400

    removed line for cleaner code

--- easy/valid-palindrome.py
+++ easy/valid-palindrome.py
@@ -5,13 +5,12 @@ class Solution:
         while l < r:
             while l < r and not self.alphaNum(s[l]):
                 l += 1
-            while r > l and not self.alphaNum(s[r]):
+            while r < l and not self.alphaNum(s[r]):
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
             l, r = l + 1, r - 1
         return True
-
 
     def alphaNum(self, c):
         return (ord('A') <= ord(c) <= ord('Z') or
